698943b0c8f0e8eceb9998b3147080615831c3f1
Jamal Frank <jamaljfrank@gmail.com>
2019-11-22 01:36:24 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,7 +1,12 @@
 class Triangle
-  attr_accessor :equilateral, :isosceles, 
+  attr_accessor :equilateral, :isosceles, :scalene, "sx, :sy, :sz"
  
   class TriangleError < StandardError
     # triangle error code
   end
+  
+  def initialize(sx, sy, sz)
+    @sx = sx
+    @sy = sy
+    @sz = sz
 end
