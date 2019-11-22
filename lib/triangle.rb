db41830db22edae8ae2afe12ac57921cd7262c3d
Jamal Frank <jamaljfrank@gmail.com>
2019-11-22 02:15:45 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,7 +11,7 @@ class Triangle
 
 
   def kind
-    if a <= 0 && b <= 0 && c <= 0 
+    if a <= 0 || b <= 0 || c <= 0 
       raise TriangleError
       elsif a == b && b == c
         :equilateral
